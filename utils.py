5b9ce5e1f92f871e6d49fd2b2564c79fad3dddee
JayLee <sk8terbo2@gmail.com>
2024-05-04 02:09:48 +0900

    add dataset

--- utils.py
+++ utils.py
@@ -106,14 +106,14 @@ def get_document_through_selenium(
         inputs = [inputs]
 
     with Pool(cpu_count()) as pool:
-        search_data = list(tqdm(pool.imap(partial(_get_document_through_selenium, n_documents=n_documents), inputs)))
+        search_data = list(tqdm(pool.imap(partial(_selenium_crawling, n_documents=n_documents), inputs)))
 
     if save_path:
         jsave(search_data, save_path, indent=indent)
 
     return search_data
 
-def _get_document_through_selenium(question, n_documents):
+def _selenium_crawling(question, n_documents):
     options = Options()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
@@ -132,6 +132,66 @@ def _get_document_through_selenium(question, n_documents):
     driver.close()
 
     return dict(question=question, document=document)
+
+def get_document_through_api(
+        query: str,
+        api_client_id: str = None,
+        api_client_secret: str = None,
+        category: Literal["blog", "news", "kin", "encyc", "cafearticle", "webkr"] = "news",
+        display: int = 20,
+        start: int = 1,
+        sort: str = "sim"
+) -> json:
+    """ Naver 검색 API를 통해 데이터를 수집합니다.
+    수집한 데이터는 모델이 generate할 때 RAG로 사용할 수 있습니다.
+    Naver 검색 API로 얻는 정보가 부정확할때가 많기 때문에 학습용도로만 이용하면 좋을 것 같습니다.
+    자세한 내용은 https://developers.naver.com/docs/serviceapi/search/blog/blog.md를 참고하세요.
+
+    Args:
+        query: 검색하려는 쿼리값
+        api_client_id: 네이버 검색 API이용을 위한 발급 받은 client_id 값
+          - 환경변수는 'NAVER_API_ID'로 설정하세요
+        api_client_secret: 네이버 검색 API이용을 위한 발급 받은 client_secret 값
+          - 환경변수는 'NAVER_API_SECRET'으로 설정하세요
+        category: 검색하려는 카테고리, 아래 카테고리로 검색이 가능합니다
+          - blog: 블로그
+          - news: 뉴스
+          - kin: 지식인
+          - encyc: 백과사전
+          - cafearticle: 카페 게시글
+          - webkr: 웹문서
+        display: 검색 결과 수 지정, default = 20
+        start: 검색 페이지 값
+        sort: 정렬값
+          - 'sim': 정확도 순으로 내림차순 정렬
+          - 'date': 날짜 순으로 내림차순 정렬
+
+    Returns: API로부터 제공받은 검색 결과 response값
+
+    """
+
+    if not (api_client_id and api_client_secret):
+        api_client_id = _getenv("NAVER_API_ID")
+        api_client_secret = _getenv("NAVER_API_SECRET")
+    if not api_client_id or not api_client_secret:
+        id_ok = "'NAVER_API_ID'" if not api_client_id else ""
+        secret_ok = "'NAVER_API_SECRET'" if not api_client_id else ""
+        raise ValueError(f"{id_ok} {secret_ok} Not setted")
+
+    url = f"https://openapi.naver.com/v1/search/{category}.json"
+    headers = {"X-Naver-Client-Id": api_client_id, "X-Naver-Client-Secret": api_client_secret}
+
+    query = requests.utils.quote(query)
+    url = url + f"?query={query}"
+    payload = dict(display=display, start=start, sort=sort)
+    payload = json.dumps(payload)
+
+    response = requests.get(url, data=payload, headers=headers)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode())
+    else:
+        return response.raise_for_status()
 
 def generate(
         model: transformers.PreTrainedModel,
@@ -162,8 +222,64 @@ def generate(
     if rag:
         search_data = get_document_through_selenium(inputs=query, n_documents=n_documents, save_path=False)
         documents = list(map(lambda x: x['document'], search_data))
-        documents, _ = similarity.sort_by_similarity(query, documents)
+        documents, scores = similarity.sort_by_similarity(query, documents)
+        documents = [d for d, s in zip(documents, scores) if s >= 0.9] # Cosine Similarity가 0.9 이상인 문서만 수집
         documents = "\n".join(documents)
+        query = query +(
+            "\n아래 documents를 참조하여 답변하세요. 먼저 제공되는 documents를 더 많이 참조하세요\n"
+            "documents:\n"
+        ) + documents
+
+    prompt = prompts.GEMMA_PROMPT.format(question=query)
+    inputs = tokenizer(prompt, add_special_tokens=False, return_tensors="pt").to(model.device)
+    outputs = model.generate(
+        **inputs,
+        repetition_penalty=repetition_penalty,
+        temperature=temperature,
+        max_new_tokens=max_new_tokens
+    )
+    completion = tokenizer.decode(outputs[0])
+
+    return completion
+
+def generate(
+        model: transformers.PreTrainedModel,
+        tokenizer: transformers.PreTrainedTokenizer,
+        query: str,
+        repetition_penalty: float = 1.5,
+        temperature: float = 0.5,
+        max_new_tokens = 256,
+        rag: bool=False,
+        rag_config:Mapping = None
+) -> str:
+    """ 모델의 generate함수입니다.
+
+    Args:
+        model: generate할 모델
+        tokenizer: 모델의 토크나이저
+        query: user가 전송하는 쿼리값
+        repetition_penalty:
+        temperature:
+        max_new_tokens:
+        rag: 네이버 검색 API를 활용한 RAG 사용 여부
+        rag_config: 네이버 검색 API에 전달될 추가 설정값
+
+    Returns: 모델이 답변하는 텍스트
+
+    """
+
+    if (rag and not rag_config):
+        raise ValueError(
+            "If you want to use RAG, pass 'rag_config' with 'api_client_id' and 'api_client_secret'"
+            "example)"
+            "rag_config={'api_client_id'}: 'your_client_id', 'api_client_scret': 'your_client_secret'"
+        )
+    if (rag and rag_config):
+        documents = get_document_through_api(query, **rag_config)
+        documents = [content['description'] for content in documents['items']]
+        documents, scores = similarity.sort_by_similarity(query, documents)
+        documents = [d for d, s in zip(documents, scores) if s >= 0.9]  # Cosine Similarity가 0.9 이상인 문서만 수집
+        documents = "\n".join(documents[:3])
         query = query +(
             "\n아래 documents를 참조하여 답변하세요. 먼저 제공되는 documents를 더 많이 참조하세요\n"
             "documents:\n"
