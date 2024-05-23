25bf3583018deba6e4f56c87410024029f221b12
JayLee <sk8terbo2@gmail.com>
2024-05-24 02:14:23 +0900

    add evaluation / fix assets

--- prompts.py
+++ prompts.py
@@ -94,3 +94,40 @@ GEMMA_TRAINING_PROMPT = """<bos><start_of_turn>user
 <start_of_turn>model
 {answer}
 """
+
+EVAL_BATTLE_PROMPT = """
+[System]
+Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user question displayed below.
+You should choose the assistant that follows the user’s instructions and answers the user’s question better.
+You should judge whether the information is fake or not based on your biggest consideration.
+Be as objective as possible.
+Avoid any position biases and ensure that the order in which the responses were presented does not influence your decision.
+Do not allow the length of the responses to influence your evaluation. Do not favor certain names of assistants.
+Be as objective as possible.
+After evaluation, output of your final verdict by strictly following this format:
+"A" if assistant A is better, "B" if assistant B is better, and "C" for a tie.
+Just write the preference only like "A", "B" or "C" without any explain.
+
+[User Question]
+{question}
+[The Start of Assistant A’s Answer]
+{answer_a}
+[The End of Assistant A’s Answer]
+[The Start of Assistant B’s Answer]
+{answer_b}
+[The End of Assistant B’s Answer]
+"""
+
+EVAL_SCORE_PROMPT="""
+[System]
+Please act as an impartial judge and evaluate the quality of the response provided by an AI assistant to the user question displayed below.
+Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of the response.
+Most of all, you should consider hallucination of the answer. Be as objective as possible.
+After evaludation, rate the response on a scale of 1 to 10 by strictly following this format: "[[rating]]", for example: "Rating: [[5]]".
+Just write the score only.
+[Question]
+{question}
+[The Start of Assistant’s Answer]
+{answer}
+[The End of Assistant’s Answer]
+"""
